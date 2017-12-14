dd309d46eccb720929c989f9ae6272d27f91af6d
Max Ackerman <max@ackermax.com>
2017-12-14 18:22:37 -0500

    added some css, fixed some html, and added dynamic button making function

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+//cute animals for the buttons!
+var topics = ["dog", "cat", "guinea pig", "bunny", "armadillo", "fox", "horse"];
+
+//make buttons on page load and each time we add an animal to the list
+function makeButtons () {
+    
+    //empty the button-holder div
+    $("#button-holder").empty();
+
+    //for loop to make each button dynamically
+    for (var i = 0; i < topics.length; i++) {
+        //make the button
+        $("<button>")
+        //give it the animal class as well as our bootstrap classes
+        .addClass("animal btn btn-lg btn-info")
+        //give the button an attribute of the animal name
+        .attr("data-name", topics[i])
+        //add button text
+        .text(topics[i])
+        //append the button to the #button-holder div
+        .appendTo("#button-holder");
+    }
+};
+
+$(document).ready(function(){
+makeButtons();
+});
