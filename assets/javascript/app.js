d91b4b6efd3cf886901ed80355609739ceb12a70
Max Ackerman <max@ackermax.com>
2017-12-14 18:48:31 -0500

    added more animal clicking functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,8 +2,8 @@
 var topics = ["dog", "cat", "guinea pig", "bunny", "armadillo", "fox", "horse"];
 
 //make buttons on page load and each time we add an animal to the list
-function makeButtons () {
-    
+function makeButtons() {
+
     //empty the button-holder div
     $("#button-holder").empty();
 
@@ -11,19 +11,19 @@ function makeButtons () {
     for (var i = 0; i < topics.length; i++) {
         //make the button
         $("<button>")
-        //give it the animal class as well as our bootstrap classes
-        .addClass("animal btn btn-lg btn-info")
-        //give the button an attribute of the animal name
-        .attr("data-name", topics[i])
-        //add button text
-        .text(topics[i])
-        //append the button to the #button-holder div
-        .appendTo("#button-holder");
+            //give it the animal class as well as our bootstrap classes
+            .addClass("animal btn btn-lg btn-info")
+            //give the button an attribute of the animal name
+            .attr("data-name", topics[i])
+            //add button text
+            .text(topics[i])
+            //append the button to the #button-holder div
+            .appendTo("#button-holder");
     }
 };
 
 //function to make animal gifs!
-function makeGIF () {
+function makeGIF() {
     //grab our animal name to push through the API
     var animal = $(this).attr("data-name");
     //make our URL to push thorugh the GIPHY API
@@ -33,27 +33,30 @@ function makeGIF () {
     $.ajax({
         url: animalURL,
         method: "GET"
-    }).done(function(r){
+    }).done(function (r) {
         console.log(r);
     })
 };
 
-$(document).ready(function(){
-//function when someone adds an animal button
-$("#animal-submit").click(function(e){
-    //stops button from doing stupid things I don't want it to do
-    e.preventDefault();
-    //grab animal choice from text box
-    var animal = $("#animal-choice").val().trim();
-    //clear text box
-    $("#animal-choice").val("");
-    //push the choice to the topics array to make buttons DYNAMICALLY (ooooooh)
-    topics.push(animal);
-    //run the button maker function to make those buttons, yo!
-    makeButtons();
-})
+$(document).ready(function () {
+    //function when someone adds an animal button
+    $("#animal-submit").click(function (e) {
+        //stops button from doing stupid things I don't want it to do
+        e.preventDefault();
+        //grab animal choice from text box
+        var animal = $("#animal-choice").val().trim();
+        //clear text box
+        $("#animal-choice").val("");
+        //push the choice to the topics array to make buttons DYNAMICALLY (ooooooh)
+        topics.push(animal);
+        //run the button maker function to make those buttons, yo!
+        makeButtons();
+    })
 
-//call function to make buttons on page load
-makeButtons();
+    //run our make animal GIF function when an animal is clicked
+    $(document).on("click", ".animal", makeGIF);
+
+    //call function to make buttons on page load
+    makeButtons();
 
 });
