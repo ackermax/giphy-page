13b1c5a2aa3b9053891124bf5535777d0a1c4b9d
Max Ackerman <max@ackermax.com>
2017-12-14 19:44:01 -0500

    added functionality for when we don't get any images returned

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -34,53 +34,61 @@ function makeGIF() {
     var animal = $(this).attr("data-name");
     //make our URL to push thorugh the GIPHY API
     var animalURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=4EGhS1dX9sZBAAACNt4FVolnUPNRLVYc&limit=10";
-    
+
     //Make that AJAX call!
     $.ajax({
         url: animalURL,
         method: "GET"
     }).done(function (r) {
-        
-        //when we got our stuff we gotta make some still images out of them
-        for (var i = 0; i < r.data.length; i++) {
-            //make a div to store the image and the rating
-           $("<div>")
-           //add class so we can get these buddies looking good
-           .addClass("inline text-center")
-           //give it an id that we can use to append the picture to later
-           .attr("id", "animal" + i)
-           //add the rating to the div
-           .html("<p>Rating: " + r.data[i].rating + "</p>")
-           //append it to the #image-holder div
-           .appendTo("#image-holder");
-            //make an image tag
-            $("<img>")
-                //give it an attribute to go back to later so we know its static
-                .attr({
-                    "is-static": "yes",
-                    //give it attributes that place the static and moving urls inside the img
-                    "static": r.data[i].images.fixed_width_still.url,
-                    "motion": r.data[i].images.fixed_width.url,
-                    //give it the static url
-                    "src": r.data[i].images.fixed_width_still.url,
-                })
-                //give it a class so we can animate it!
-                .addClass("animal-image")
-                //prepend this to the div with the rating we just made
-                .prependTo("#animal" + i);
-            
+
+        //if there's no images returned we should let the user know
+        if (r.data.length == 0) {
+            alert("Sorry, but it appears we couldn't find any images with the name " + animal + ". Please check to see if your spelling is off, make a new button, and try again!")
+        }
+
+        //otherwise if we got images back we're good to go!
+        else {
+            //when we got our stuff we gotta make some still images out of them
+            for (var i = 0; i < r.data.length; i++) {
+                //make a div to store the image and the rating
+                $("<div>")
+                    //add class so we can get these buddies looking good
+                    .addClass("inline text-center")
+                    //give it an id that we can use to append the picture to later
+                    .attr("id", "animal" + i)
+                    //add the rating to the div
+                    .html("<p>Rating: " + r.data[i].rating + "</p>")
+                    //append it to the #image-holder div
+                    .appendTo("#image-holder");
+                //make an image tag
+                $("<img>")
+                    //give it an attribute to go back to later so we know its static
+                    .attr({
+                        "is-static": "yes",
+                        //give it attributes that place the static and moving urls inside the img
+                        "static": r.data[i].images.fixed_width_still.url,
+                        "motion": r.data[i].images.fixed_width.url,
+                        //give it the static url
+                        "src": r.data[i].images.fixed_width_still.url,
+                    })
+                    //give it a class so we can animate it!
+                    .addClass("animal-image")
+                    //prepend this to the div with the rating we just made
+                    .prependTo("#animal" + i);
+
+            }
         }
     })
 };
 
 //function to make our GIFs animate!
-function gifAnimate () {
+function gifAnimate() {
     //if it's static
     if ($(this).attr("is-static") == "yes") {
-    //change the static url to the animated one!
-    $(this).attr("src", $(this).attr("motion"));
-    //change is-static to "no"
-    $(this).attr("is-static", "no");
+        //change the static url to the animated one!
+        $(this).attr("src", $(this).attr("motion"));
+        //change is-static to "no"
+        $(this).attr("is-static", "no");
     }
     //otherwise if it's not static
     else {
